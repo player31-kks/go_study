8497fbf4077592b55bd568224f1269831940b765
player31-kks <keumks789@naver.com>
2023-10-02 11:52:10 +0900

    [Add] insertstion sort

--- collection/basic-sort/swap.go
+++ collection/basic-sort/swap.go
@@ -16,6 +16,46 @@ func Sort(arr []int) {
 	}
 }
 
+func SelectionSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		minIdx := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[minIdx] > arr[j] {
+				minIdx = j
+			}
+		}
+		Swap(&arr[i], &arr[minIdx])
+	}
+}
+
+func BubbleSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		changed := false
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				Swap(&arr[j], &arr[j+1])
+				changed = true
+			}
+		}
+		if !changed {
+			return
+		}
+	}
+}
+
+func InsertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
+
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j = j - 1
+		}
+		arr[j+1] = key
+	}
+}
+
 func CheckSort(arr []int) bool {
 	for i := 0; i < len(arr)-1; i++ {
 		if arr[i] > arr[i+1] {
--- collection/basic-sort/swap_test.go
+++ collection/basic-sort/swap_test.go
@@ -40,10 +40,29 @@ func TestSort3(t *testing.T) {
 			for k := 0; k < 3; k++ {
 				arr := make([]int, 0, 3)
 				arr = append(arr, i, j, k)
-				fmt.Println("before", CheckSort(arr))
-				Sort(arr)
+				SelectionSort(arr)
 				fmt.Println("after", CheckSort(arr))
 			}
 		}
 	}
+}
+
+func TestSort4(t *testing.T) {
+	arr := []int{8, 3, 2, 5, 1, 1, 2, 5, 8, 9}
+	SelectionSort(arr)
+	fmt.Println(CheckSort(arr))
+}
+
+func TestSort5(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5, 6}
+	BubbleSort(arr)
+	fmt.Println(arr)
+	fmt.Println(CheckSort(arr))
+}
+
+func TestSort6(t *testing.T) {
+	arr := []int{5, 1, 3, 4, 6}
+	InsertionSort(arr)
+	fmt.Println(arr)
+	fmt.Println(CheckSort(arr))
 }
